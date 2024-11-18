ccde570fc0a8587a55f1288555ca3e457bbf61e2
uliana <kolosovichulia@mail.ru>
2024-11-18 23:14:33 +0300

    base project

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-import 'package:quiz_flutter_app/main_container.dart';
+import 'package:quiz_flutter_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-    body: MainContainer(Colors.purple),
-  )));
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(69, 27, 143, 1),
+                Color.fromRGBO(109, 69, 180, 1),
+                Color.fromRGBO(112, 68, 187, 1),
+                Color.fromRGBO(117, 80, 180, 1),
+                Color.fromRGBO(134, 111, 175, 1),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
